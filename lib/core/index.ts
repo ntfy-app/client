adaf63613554bf7b7651691b573ceaff97175b4a
Martin Nirtl <martin@nirtl.rocks>
2020-07-31 21:53:45 +0200

    add jsdocs

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -12,6 +12,7 @@ interface ClientInitOptions {
 
 /**
  * The Notify App's javascript client to be used for sending events, logs and status messages.
+ * @class
  */
 export class Client {
   api: string
@@ -20,8 +21,15 @@ export class Client {
   label?: string
 
   /**
-   * @param secret The app's client-secret
-   * @param options Options object to configure the client instance
+   * @param {string} secret The app's client-secret
+   * @param {ClientInitOptions} [options] Options object to configure the client instance
+   * @param {string} [options.api] Change the default API
+   * @param {string} [options.label] Label the client instance (label will be sent via the metadata object)
+   * @example
+   *  new Client('clientse_dev_abcdefghijkl...xyz', {
+   *    label: 'instance-2',
+   *  })
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   constructor(secret: string, options?: ClientInitOptions) {
     this.api = options?.api || 'https://ntfy.live/graphql'
@@ -50,13 +58,18 @@ export class Client {
 
   /**
    * Sends an event message to the Notify App
-   * @param {string} eventMessage The event message object
+   * @param eventMessage The event message object
+   * @param {string} eventMessage.title Title of the event
+   * @param {string} [eventMessage.category] Optional event category (GENERAL, MAIL, PAYMENT, BLOG, etc.)
+   * @param {string} eventMessage.message Message
+   * @param {Object} [eventMessage.metadata] Optional metadata object
    * @example
    *  client.event({
    *    title: 'New Blogpost',
    *    message: `Just published the new post '${post}'!`,
    *    category: 'POST',
    *  })
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   event(eventMessage: EventMessage) {
     eventMessage.category = eventMessage.category && checkEventCategory(eventMessage.category)
@@ -71,15 +84,18 @@ export class Client {
   }
 
   /**
-   * log
    * Sends a log message to the Notify App
    * @param logMessage The log message object
+   * @param logMessage.level Log-level (INFO, WARN, ERROR, FATAL)
+   * @param {string} logMessage.message Message
+   * @param {Object} [logMessage.metadata] Optional metadata object
    * @example
    *  client.log({
    *    level: 'ERROR',
    *    message: `Could not reconnect to database!`,
    *    metadata: { retries: 10 },
    *  })
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   log(logMessage: LogMessage) {
     logMessage.metadata = processMetadata(logMessage.metadata, this.label)
@@ -93,54 +109,57 @@ export class Client {
   }
 
   /**
-   * info
    * Sends a log message to the Notify App
-   * @param message The info-log message
-   * @param metadata JSON object containing metadata
+   * @param {string} message The info-log message
+   * @param {Object} [metadata] Optional metadata object
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   info(message: string, metadata?: any) {
     this.log({ message, level: LogLevel.Info, metadata })
   }
 
   /**
-   * warn
    * Sends a log message to the Notify App
-   * @param message The warn-log message
-   * @param metadata JSON object containing metadata
+   * @param {string} message The warn-log message
+   * @param {Object} [metadata] Optional metadata object
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   warn(message: string, metadata?: any) {
     this.log({ message, level: LogLevel.Warn, metadata })
   }
 
   /**
-   * error
    * Sends a log message to the Notify App
-   * @param message The error-log message
-   * @param metadata JSON object containing metadata
+   * @param {string} message The error-log message
+   * @param {Object} [metadata] Optional metadata object
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   error(message: string, metadata?: any) {
     this.log({ message, level: LogLevel.Error, metadata })
   }
 
   /**
-   * fatal
    * Sends a log message to the Notify App
-   * @param message The fatal-log message
-   * @param metadata JSON object containing metadata
+   * @param {string} message The fatal-log message
+   * @param {Object} [metadata] Optional metadata object
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   fatal(message: string, metadata?: any) {
     this.log({ message, level: LogLevel.Fatal, metadata })
   }
 
   /**
-   * status
    * Sends the present status of your application to the Notify App
    * @param statusMessage The status message object
+   * @param {string} statusMessage.state Actual state (UP, DOWN, CRITICAL, SHUTDOWN, UNKNOWN)
+   * @param {string} statusMessage.message Message
+   * @param {Object} [statusMessage.metadata] Optional metadata object
    * @example
    *  client.status({
    *    state: 'UP',
    *    message: `App is up and running!`,
    *  })
+   * @see Read the {@link http://github.com/ntfy-app/client|docs}.
    */
   status(statusMessage: StatusMessage) {
     statusMessage.metadata = processMetadata(statusMessage.metadata, this.label)
